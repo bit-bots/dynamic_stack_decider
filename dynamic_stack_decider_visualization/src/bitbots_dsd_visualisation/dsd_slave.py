6f2b66417f06f0100731e84246aa291eaea34b05
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2018-12-13 11:40:25 +0100

    Inital code commit

---
+++ dynamic_stack_decider_visualization/src/bitbots_dsd_visualisation/dsd_slave.py
@@ -0,0 +1,187 @@
+import re
+import uuid
+from typing import List, Tuple
+
+import rospy
+from std_msgs.msg import String
+
+from bitbots_dsd.abstract_stack_element import AbstractStackElement
+from bitbots_dsd.dsd import DSD
+
+import pydot
+
+from bitbots_dsd.tree import AbstractTreeElement, ActionTreeElement, DecisionTreeElement
+
+
+class ParseException(Exception):
+    pass
+
+
+class DsdSlave(DSD):
+
+    def __init__(self, debug_topic):
+        super().__init__(None, None)
+
+        self.debug_subscriber = rospy.Subscriber(debug_topic, String, self.subscriber_callback, queue_size=10)
+        self.__cached_msg = None
+        self.__cached_dotgraph = None
+        self.initialized = False
+
+    def _init_element(self, element, parameters=None):
+        """
+        We do not initialize anything on the slave
+        """
+        return None
+
+    def update(self, reevaluate=True):
+        """
+        The DSD slave does not execute any code
+        """
+        pass
+
+    def close(self):
+        self.debug_subscriber.unregister()
+
+    def __parse_remote_msg(self, remaining_msg, parent_element):
+        """
+        Recursively parse the remaining part of a remote DSDs state description message
+        :arg parent_element: The Tree element which is the parent of the newly parsed element
+        :type parent_element: AbstractTreeElement
+        :type remaining_msg: str
+        """
+        # match the complete expression of one element on the stack
+        match_all = re.search('\|-.*?;', remaining_msg)
+        if match_all is None:  # end of recursion
+            return
+        match_all = match_all.group()
+
+        # test edge case
+        if isinstance(parent_element, ActionTreeElement):
+            raise ParseException('The remote DSD sent further elements which seem to be on the stack'
+                                 'but the local DSD tree has already reached an ActionElement')
+
+        match_type = re.search('\$|@|:abstract:', match_all).group()
+
+        # test edge case
+        if match_type == ':abstract:':
+            raise ParseException('The remote DSD seems to have an abstract element on its stack')
+
+        match_reason = re.search('\|-.*?->', match_all).group()[2:-2]
+        match_class = re.search(f'->..*?\[', match_all).group()[3:-1]
+        match_data = re.search('\[.*\];', match_all).group()[1:-2]  # TODO Show data
+
+        # Search for the correct child element
+        #   Since the remote DSD should have the same tree it HAS to be directly under the parent_element node
+        try:
+            element = parent_element.get_child(match_reason)
+        except KeyError:
+            raise ParseException(f'The matched activating_result {match_reason} could not be found in local tree'
+                                 f' (from parent {parent_element})')
+
+        self.push(element)
+
+        self.__parse_remote_msg(remaining_msg.replace(match_all, ''), element)
+
+    def subscriber_callback(self, msg: String):
+        # abort if the dsd is not fully loaded yet
+        if not self.initialized:
+            return
+
+        msg = msg.data
+
+        # abort if nothing changed
+        if msg == self.__cached_msg:
+            return
+        self.__cached_dotgraph = None
+
+        # extract the root stack element
+        match_root_all = re.search('^\|-None->.*?;', msg).group()
+        self.set_start_element(self.tree.root_element)  # TODO actually choose the correct start element
+
+        # parse the remaining stack (without root element)
+        self.__parse_remote_msg(msg.replace(match_root_all, ''), self.start_element)
+
+        # cache the message so we dont have to reprocess it
+        self.__cached_msg = msg
+
+    @staticmethod
+    def __error_dotgraph() -> pydot.Dot:
+        dot = pydot.Dot(graph_type='digraph')
+
+        param_debug_active = rospy.get_param("/debug_active", False)
+
+        uid1 = str(uuid.uuid4())
+        dot.add_node(pydot.Node(uid1, label="I have not received anything from the dsd yet"))
+
+        uid2 = str(uuid.uuid4())
+        dot.add_node(pydot.Node(uid2, label="Please make sure that\n"
+                                            "- The appropriate dsd is started\n"
+                                            "- You are connected to the same roscore\n"
+                                            f"- param /debug_active is True (for me it is {param_debug_active})"))
+
+        dot.add_edge(pydot.Edge(uid1, uid2))
+
+        return dot
+
+    def __stack_to_dotgraph(self, stack: List[Tuple[AbstractTreeElement, AbstractStackElement]], dot: pydot.Dot) -> Tuple[pydot.Dot, str]:
+        """
+        Recursively modify dot to include every element of the stack
+        """
+        element, _ = stack[0]
+
+        # Determine correct shape
+        if isinstance(element, DecisionTreeElement):
+            shape = 'box'
+        else:
+            shape = 'ellipse'
+
+        # Create node in graph
+        uid = str(uuid.uuid4())
+        node = pydot.Node(uid, label=element.name, shape=shape)
+        dot.add_node(node)
+
+        # Append all direct children to graph
+        # Also append all children which are on the stack to the graph
+        if isinstance(element, DecisionTreeElement):
+            for activating_result, child in element.children.items():
+
+                # Since this child is on the stack as well, it should be represented completely
+                if len(stack) > 1 and activating_result == stack[1][0].activation_reason:
+                    dot, child_uid = self.__stack_to_dotgraph(stack[1:], dot)
+
+                # Draw this child simple because we want to show direct children of elements
+                else:
+                    # Determine shape of child
+                    if isinstance(child, DecisionTreeElement):
+                        child_shape = 'box'
+                    else:
+                        child_shape = 'ellipse'
+
+                    # Create child node in graph
+                    child_uid = str(uuid.uuid4())
+                    child_node = pydot.Node(child_uid, label=child.name, shape=child_shape, color='lightgray')
+                    dot.add_node(child_node)
+
+                # Connect the child to the parent element
+                edge = pydot.Edge(uid, child_uid, label=activating_result)
+                dot.add_edge(edge)
+
+        return dot, uid
+
+    def to_dotgraph(self) -> pydot.Dot:
+        """
+        Represent the current stack as dotgraph
+        """
+        # Return cached result if available
+        if self.__cached_dotgraph is not None:
+            return self.__cached_dotgraph
+
+        # Return special error graph which shows error information when no data was received
+        if self.__cached_msg is None:
+            return self.__error_dotgraph()
+
+        dot = pydot.Dot(graph_type='digraph')
+        dot, uid = self.__stack_to_dotgraph(self.stack, dot)
+
+        self.__cached_dotgraph = dot
+        return dot
---
+++ dynamic_stack_decider_visualization/src/bitbots_dsd_visualisation/dsd_visualisation_plugin.py
@@ -0,0 +1,269 @@
+# Copyright (c) 2011, Dirk Thomas, TU Darmstadt
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#   * Neither the name of the TU Darmstadt nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import uuid
+
+import pydot
+import rospkg
+import yaml
+from bitbots_dsd_visualisation.dsd_slave import DsdSlave
+from python_qt_binding import loadUi
+from python_qt_binding.QtCore import Qt
+from python_qt_binding.QtGui import QIcon, QPainter, QStandardItemModel
+from python_qt_binding.QtWidgets import QFileDialog, QGraphicsScene, QWidget
+from qt_dotgraph.dot_to_qt import DotToQtGenerator
+from qt_dotgraph.pydotfactory import PydotFactory
+from rqt_gui_py.plugin import Plugin
+
+from .interactive_graphics_view import InteractiveGraphicsView
+
+
+def parse_locations_yaml() -> list:
+    rp = rospkg.RosPack()
+    path = os.path.join(rp.get_path('bitbots_dsd_visualisation'), 'config', 'locations.yaml')
+    with open(path, 'r') as f:
+        return yaml.load(f)['locations']
+
+
+class DsdVizPlugin(Plugin):
+
+    def __init__(self, context):
+        super(DsdVizPlugin, self).__init__(context)
+
+        self._initialized = False  # This gets set to true once the plugin hast completely finished loading
+
+        # Ensure startup state
+        self.freeze = False  # Controls whether the state should be updated from remote
+        self.locations = parse_locations_yaml()
+        self.dsd = None  # type: DsdSlave
+        self._init_plugin(context)
+
+        # Performance optimization variables
+        self._prev_dotgraph = None
+        self._prev_QItemModel = None
+
+    def _init_plugin(self, context):
+        self.setObjectName("DSD-Visualization")
+
+        self._widget = QWidget()
+        self._widget.setObjectName(self.objectName())
+
+        # load qt ui definition from file
+        rp = rospkg.RosPack()
+        ui_file = os.path.join(rp.get_path("bitbots_dsd_visualisation"), "resource", "StackmachineViz.ui")
+        loadUi(ui_file, self._widget, {"InteractiveGraphicsView": InteractiveGraphicsView})
+
+        # initialize qt scene
+        self._scene = QGraphicsScene()
+        self._scene.setBackgroundBrush(Qt.white)
+        self._widget.graphics_view.setScene(self._scene)
+
+        # Bind fit-in-view button
+        self._widget.fit_in_view_push_button.setIcon(QIcon.fromTheme('zoom-original'))
+        self._widget.fit_in_view_push_button.pressed.connect(self.fit_in_view)
+
+        # Fit-in-view on checkbox toggle
+        self._widget.auto_fit_graph_check_box.toggled.connect(self.fit_in_view)
+
+        # Freezing
+        def toggle_freeze():
+            self.freeze = not self.freeze
+            self.refresh()
+
+        self._widget.freeze_push_button.toggled.connect(toggle_freeze)
+
+        # Exporting and importing
+        self._widget.save_as_svg_push_button.pressed.connect(self.save_svg_to_file)
+
+        # Fill choices for dsd_selector and bind on_select
+        self._widget.dsd_selector_combo_box.addItem("Select DSD...")
+        for choice in self.locations:
+            self._widget.dsd_selector_combo_box.addItem(choice['display_name'])
+        self._widget.dsd_selector_combo_box.currentTextChanged.connect(self.set_dsd)
+
+        context.add_widget(self._widget)
+
+        # Start a timer that calls back every 100 ms
+        self._timer_id = self.startTimer(100)
+
+    def save_settings(self, plugin_settings, instance_settings):
+        super(DsdVizPlugin, self).save_settings(plugin_settings, instance_settings)
+
+        instance_settings.set_value('auto_fit_graph_check_box_state', self._widget.auto_fit_graph_check_box.isChecked())
+        instance_settings.set_value('highlight_connections_check_box_state',
+                                    self._widget.highlight_connections_check_box.isChecked())
+
+    def restore_settings(self, plugin_settings, instance_settings):
+        super(DsdVizPlugin, self).restore_settings(plugin_settings, instance_settings)
+
+        self._widget.auto_fit_graph_check_box.setChecked(
+            instance_settings.value('auto_fit_graph_check_box_state', True) in [True, 'true'])
+        self._widget.highlight_connections_check_box.setChecked(
+            instance_settings.value('highlight_connections_check_box_state', True) in [True, 'true'])
+
+        self._initialized = True
+        self.refresh()
+
+    def save_svg_to_file(self):
+        file_name, _ = QFileDialog.getSaveFileName(self._widget, self.tr("Save as SVG"), "stackmachine.svg",
+                                                   self.tr("Scalable Vector Graphic (*.svg)"))
+
+        if file_name is not None and file_name != "":
+            generator = QSvgGenerator()
+            generator.setFileName(file_name)
+            generator.setSize((self._scene.sceneRect().size() * 2.0).toSize())
+
+            painter = QPainter(generator)
+            painter.setRenderHint(QPainter.Antialiasing)
+            self._scene.render(painter)
+            painter.end()
+
+    def timerEvent(self, timer_event):
+        """This gets called by QT whenever the timer ticks"""
+
+        if not self.freeze:
+            self.refresh()
+
+        # Automatically fit in view if the checkbox is checked
+        if self._widget.auto_fit_graph_check_box.isChecked():
+            self.fit_in_view()
+
+    def fit_in_view(self):
+        self._widget.graphics_view.fitInView(self._scene.itemsBoundingRect(), Qt.KeepAspectRatio)
+
+    def refresh(self):
+        """Refresh the complete drawn representation"""
+
+        if not self._initialized:
+            self._render_messages("The plugin is not yet completely initialized. Please wait...")
+
+        elif self.dsd is None:
+            self._render_messages("No DSD selected")
+
+        else:
+            self._render_dotgraph(self.dsd.to_dotgraph())
+            # self._render_debug_data(self.stackmachine_state.to_QItemModel(self._scene))
+
+    def _render_messages(self, *messages):
+        """Render simple messages on the canvas"""
+
+        msg_dot = pydot.Dot()
+
+        for msg in messages:
+            msg_dot.add_node(pydot.Node(str(uuid.uuid4()), label=str(msg)))
+
+        self._render_dotgraph(msg_dot)
+        self._render_debug_data(QStandardItemModel(self._scene))
+
+    def _render_dotgraph(self, dotgraph):
+        """
+        Render the specified dotgraph on canvas
+
+        :type dotgraph: pydot.Dot
+        """
+
+        # Only redraw when the graph differs from the previous one
+        if self._prev_dotgraph == dotgraph:
+            return
+        else:
+            self._prev_dotgraph = dotgraph
+
+        self._scene.clear()
+
+        if self._widget.highlight_connections_check_box.isChecked():
+            highlight_level = 3
+        else:
+            highlight_level = 1
+
+        # Generate qt items from dotcode
+        dotcode = PydotFactory().create_dot(dotgraph)
+        nodes, edges = DotToQtGenerator().dotcode_to_qt_items(dotcode, highlight_level,
+                                                              same_label_siblings=False)
+
+        # Add generated items to scene
+        for node_item in nodes:
+            self._scene.addItem(nodes.get(node_item))
+        for edge_items in edges:
+            for edge_item in edges.get(edge_items):
+                edge_item.add_to_scene(self._scene)
+
+        self._scene.setSceneRect(self._scene.itemsBoundingRect())
+
+    def _render_debug_data(self, qitem_model):
+        """Render debug data in the tree view on the right side of the scene"""
+
+        # Only redraw when the itemmodel differs from the previous one
+        if self._prev_QItemModel == qitem_model:
+            return
+        else:
+            self._prev_QItemModel = qitem_model
+
+        self._widget.stack_prop_tree_view.setModel(qitem_model)
+        self._widget.stack_prop_tree_view.expandAll()
+
+    def set_dsd(self, name):
+        """
+        Set the target dsd
+
+        :param name: display_name of any dsd in the locations.yaml
+        """
+
+        # close debug connection of old dsd
+        if self.dsd is not None:
+            self.dsd.close()
+
+        if name == 'Select DSD...':
+            self.dsd = None
+            return
+
+        # Search for dsd_data in locations.yaml
+        for i in self.locations:
+            if i['display_name'] == name:
+                dsd_data = i
+                break
+        else:
+            raise ValueError(f'no dsd with name {name} found')
+
+        # Figure out full paths with the help of rospkg
+        rospack = rospkg.RosPack()
+        dsd_path = rospack.get_path(dsd_data['package'])
+        actions_path = os.path.join(dsd_path, dsd_data['relative_action_path'])
+        decisions_path = os.path.join(dsd_path, dsd_data['relative_decision_path'])
+        behaviour_path = os.path.join(dsd_path, dsd_data['relative_dsd_path'])
+
+        # Initialize dsd instance
+        dsd = DsdSlave(dsd_data['debug_topic'])
+        dsd.register_actions(actions_path)
+        dsd.register_decisions(decisions_path)
+        dsd.load_behavior(behaviour_path)
+        dsd.initialized = True
+
+        self.dsd = dsd
