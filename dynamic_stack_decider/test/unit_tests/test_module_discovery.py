a9dec66ac35f6ca9cb7b958a9d8570a2a62fd522
Finn-Thorben Sell <dev@finn-thorben.me>
2021-04-28 14:27:39 +0200

    add tests for registering actions and decisions in dsd

--- dynamic_stack_decider/test/unit_tests/test_module_discovery.py
+++ dynamic_stack_decider/test/unit_tests/test_module_discovery.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 from dynamic_stack_decider.abstract_action_element import AbstractActionElement
-from dynamic_stack_decider.dsd import discover_elements
+from dynamic_stack_decider.dsd import discover_elements, DSD
 from bitbots_test.test_case import TestCase
+
+project_dir = Path(__file__).parent / "test_project"
 
 
 class TestAction(AbstractActionElement):
@@ -11,32 +13,30 @@ class TestAction(AbstractActionElement):
 
 
 class ModuleDiscoveryTestCase(TestCase):
-    project_dir = Path(__file__).parent / "test_project"
-
     def test_single_file(self):
         # execution
-        elements = discover_elements(str(self.project_dir / "actions" / "test_action.py"))
+        elements = discover_elements(str(project_dir / "actions" / "test_action.py"))
 
         # verification
         self.assertEqual(list(elements.keys()), ["TestAction"])
 
     def test_explicit_init_py_file(self):
         # execution
-        elements = discover_elements(str(self.project_dir / "actions" / "__init__.py"))
+        elements = discover_elements(str(project_dir / "actions" / "__init__.py"))
 
         # verification
         self.assertEqual(list(elements.keys()), ["InitAction"])
 
     def test_directory(self):
         # execution
-        elements = discover_elements(str(self.project_dir / "actions"))
+        elements = discover_elements(str(project_dir / "actions"))
 
         # verification
         self.assertEqual(list(elements.keys()), ["InitAction", "AmbiguousAction", "TestAction"])
 
     def test_directory_traversal(self):
         # execution
-        elements = discover_elements(str(self.project_dir))
+        elements = discover_elements(str(project_dir))
 
         # verification
         self.assertEqual(list(elements.keys()), ["InitAction", "AmbiguousAction", "TestAction", "InitDecision", "AmbiguousDecision", "TestDecision"])
@@ -47,6 +47,41 @@ class ModuleDiscoveryTestCase(TestCase):
 
         # verification
         self.assertEqual(list(elements.keys()), ["TestAction"])
+
+
+class ModuleRegistrationTestCase(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.dsd = DSD({})
+
+    def test_register_actions(self):
+        with self.subTest("project_dir"):
+            # execution
+            self.dsd.register_actions(str(project_dir))
+            # verification
+            self.assertEqual(list(self.dsd.actions.keys()), ["InitAction", "AmbiguousAction", "TestAction"])
+
+        self.setUp()
+        with self.subTest("project_dir / actions"):
+            # execution
+            self.dsd.register_actions(str(project_dir / "actions"))
+            # verification
+            self.assertEqual(list(self.dsd.actions.keys()), ["InitAction", "AmbiguousAction", "TestAction"])
+
+    def test_register_decisions(self):
+        with self.subTest("project_dir"):
+            # execution
+            self.dsd.register_decisions(str(project_dir))
+            # verification
+            self.assertEqual(list(self.dsd.decisions.keys()), ["InitDecision", "AmbiguousDecision", "TestDecision"])
+
+        self.setUp()
+        with self.subTest("project_dir / decisions"):
+            # execution
+            self.dsd.register_decisions(str(project_dir / "decisions"))
+            # verification
+            self.assertEqual(list(self.dsd.decisions.keys()), ["InitDecision", "AmbiguousDecision", "TestDecision"])
+
 
 
 if __name__ == "__main__":
