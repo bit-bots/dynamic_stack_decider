b4ee7705298371ea340b0dddd79c771a190f8f31
Florian Vahl <git@flova.de>
2022-10-17 23:43:49 +0200

    Fix dsd viz warnings

--- dynamic_stack_decider_visualization/setup.py
+++ dynamic_stack_decider_visualization/setup.py
@@ -10,6 +10,8 @@ setup(
     data_files=[
         ('share/' + package_name + '/resource',
             ['resource/StackmachineViz.ui']),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
         ('share/' + package_name + "/config",
