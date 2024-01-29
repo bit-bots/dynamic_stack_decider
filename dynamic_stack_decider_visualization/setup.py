7c93d900e75354fae090a2adafd95bad90946b3e
Florian Vahl <git@flova.de>
2024-01-29 06:47:51 +0100

    Remove unused config install path

--- dynamic_stack_decider_visualization/setup.py
+++ dynamic_stack_decider_visualization/setup.py
@@ -11,8 +11,7 @@ setup(
         ("share/" + package_name + "/resource", ["resource/StackmachineViz.ui"]),
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        ("share/" + package_name, ["plugin.xml"]),
-        ("share/" + package_name + "/config", glob.glob("config/*.yaml")),
+        ("share/" + package_name, ["plugin.xml"])
     ],
     install_requires=["setuptools"],
     zip_safe=True,
