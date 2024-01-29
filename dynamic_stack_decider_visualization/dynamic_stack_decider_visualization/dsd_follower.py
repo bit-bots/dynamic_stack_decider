05c730b60e95720c5b9c2b2d86d897a607d5e955
Florian Vahl <git@flova.de>
2024-01-29 22:51:13 +0100

    Fix formatting

--- dynamic_stack_decider_visualization/dynamic_stack_decider_visualization/dsd_follower.py
+++ dynamic_stack_decider_visualization/dynamic_stack_decider_visualization/dsd_follower.py
@@ -216,9 +216,13 @@ class DsdFollower:
         """
         # Sanity check
         if stack_root is not None:
-            assert stack_root["type"] == subtree_root["type"], "The stack root type and the tree root type do not match"
+            assert (
+                stack_root["type"] == subtree_root["type"]
+            ), f"The stack element type and the tree element type do not match ({stack_root['type']} vs. {subtree_root['type']})"
             if stack_root["type"] != "sequence":
-                assert stack_root["name"] == subtree_root["name"], "The stack root name and the tree root name do not match"
+                assert (
+                    stack_root["name"] == subtree_root["name"]
+                ), f"The stack element name and the tree element name do not match ({stack_root['name']} vs. {subtree_root['name']})"
 
         # Generate dot node for the root and mark it as active if it is on the stack
         dot_node = DsdFollower._dot_node_from_tree_element(subtree_root, stack_root)
